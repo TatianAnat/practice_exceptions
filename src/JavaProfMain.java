86f6390d69ee8c38b4a6f028cd85fccbeac5cbbc
TatianAnat <TatianAnat@yandex.ru>
2025-06-27 22:52:05 +0400

    practice exceptions

--- src/JavaProfMain.java
+++ src/JavaProfMain.java
@@ -1,14 +1,19 @@
 public class JavaProfMain {
     public static void main(String[] args) {
         System.out.println("Skypro Uni - Java Profession start!");
-        toThrowOrNotToThrow(true);
-        toThrowOrNotToThrow(false);
+        try {
+            toThrowOrNotToThrow(false);
+            toThrowOrNotToThrow(true);
+        } catch (Exception e) {
+            throw new RuntimeException(e); //оборачиваем проверяемое исключение в непроверяемое
+            //т.о. не теряем исключение, а пробрасываем наверх и сохраняем всю информацию о том исключении, которое было выбрашено первоначально
+        }
     }
 
-    private static void toThrowOrNotToThrow(boolean exc) {
+    private static void toThrowOrNotToThrow(boolean exc) throws Exception {
         System.out.println("JavaProfMain.toThrowOrNotToThrow start");
         if (exc) {
-            throw new RuntimeException(); //основное не проверяемое исключение
+            throw new Exception(); //основное не проверяемое исключение
         }
         System.out.println("JavaProfMain.toThrowOrNotToThrow end");
     }
