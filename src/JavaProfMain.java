95705992d1dbe2feba91e1ed3eba9b90e89dfc6c
TatianAnat <TatianAnat@yandex.ru>
2025-06-27 23:21:54 +0400

    practice exceptions_try_catch

--- src/JavaProfMain.java
+++ src/JavaProfMain.java
@@ -1,19 +1,26 @@
+import java.util.IllegalFormatConversionException;
+import java.util.IllegalFormatFlagsException;
+
 public class JavaProfMain {
     public static void main(String[] args) {
         System.out.println("Skypro Uni - Java Profession start!");
-        try {
-            toThrowOrNotToThrow(false);
-            toThrowOrNotToThrow(true);
-        } catch (Exception e) {
-            throw new RuntimeException(e); //оборачиваем проверяемое исключение в непроверяемое
-            //т.о. не теряем исключение, а пробрасываем наверх и сохраняем всю информацию о том исключении, которое было выбрашено первоначально
-        }
-    }
-
+        multiCatch();
+}
     private static void toThrowOrNotToThrow(boolean exc) throws Exception {
         System.out.println("JavaProfMain.toThrowOrNotToThrow start");
         if (exc) {
             throw new Exception(); //основное не проверяемое исключение
+        }
+        System.out.println("JavaProfMain.toThrowOrNotToThrow end");
+    }
+
+    private static void multiCatch(){
+        try {
+            System.out.printf("%#%f",1);
+        } catch (IllegalFormatConversionException e) {
+            System.out.println("Caught exc");
+        } catch (IllegalFormatFlagsException e) {
+            System.out.println("Caught exc");
         }
         System.out.println("JavaProfMain.toThrowOrNotToThrow end");
     }
